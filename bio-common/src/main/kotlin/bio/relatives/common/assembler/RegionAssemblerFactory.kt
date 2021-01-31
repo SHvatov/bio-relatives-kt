b448c29c6985dc070db1015fd133661a16ea37ee
Sergey HVatov <ishvatov@gmail.com>
2021-01-31 21:26:57 +0300

    Added NaiveRegionAssemblerFactory implementation

--- bio-common/src/main/kotlin/bio/relatives/common/assembler/RegionAssemblerFactory.kt
+++ bio-common/src/main/kotlin/bio/relatives/common/assembler/RegionAssemblerFactory.kt
@@ -4,5 +4,8 @@ package bio.relatives.common.assembler
  * @author shvatov
  */
 interface RegionAssemblerFactory {
+    /**
+     * Creates an instance of the [RegionAssembler] defines in the plugged in module.
+     */
     fun create(): RegionAssembler
 }

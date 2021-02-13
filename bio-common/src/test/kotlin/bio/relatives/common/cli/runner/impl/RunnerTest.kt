fca0dd6a3fb162acc7ec1fbad0f8463fc05c6bb7
MarchenkoVladislav <vlad25091999@mail.ru>
2021-02-13 08:20:37 +0300

    WIP

---
+++ bio-common/src/test/kotlin/bio/relatives/common/cli/runner/impl/RunnerTest.kt
@@ -0,0 +1,13 @@
+package bio.relatives.common.cli.runner.impl
+
+import org.junit.Test
+
+/**
+ * @author Created by Vladislav Marchenko on 11.02.2021
+ */
+class RunnerTest {
+
+    @Test
+    fun run() {
+    }
+}

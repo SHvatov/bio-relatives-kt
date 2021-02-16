f8182b9aea43e1155cea071ea3eb82c6b6b2c4f0
Sergey HVatov <ishvatov@gmail.com>
2021-02-16 22:57:10 +0300

    Implemented GenomeAssemblerImpl with TaskProcessor and Objects pool

--- bio-common/src/main/kotlin/bio/relatives/common/assembler/GenomeAssembler.kt
+++ bio-common/src/main/kotlin/bio/relatives/common/assembler/GenomeAssembler.kt
@@ -1,17 +1,28 @@
 package bio.relatives.common.assembler
 
+import bio.relatives.common.model.Feature
 import bio.relatives.common.model.RegionBatch
+import com.shvatov.processor.CoroutineParentScopeAware
+import com.shvatov.processor.CoroutineScopeAware
+import com.shvatov.processor.data.Task
+import com.shvatov.processor.data.TaskResult
 import kotlinx.coroutines.channels.ReceiveChannel
 
 /**
  * @author shvatov
  */
-interface GenomeAssembler : AutoCloseable {
+interface GenomeAssembler : AutoCloseable, CoroutineScopeAware, CoroutineParentScopeAware {
     /**
      * Assembles the genome based on the files provided on initialization.
      * Starts a separate coroutine, which is responsible for the assembly of the genomes.
      * Returns a [ReceiveChannel], which contains [RegionBatch], which represent
      * the result of assembly of one region. All regions represent the whole genome.
      */
-    fun assemble(): ReceiveChannel<RegionBatch>
+    fun assemble(): ReceiveChannel<GenomeAssemblyResult>
 }
+
+/**
+ * Type aliases for the genome assembly task and its result.
+ */
+typealias GenomeAssemblyTask = Task<Feature, RegionBatch>
+typealias GenomeAssemblyResult = TaskResult<RegionBatch>
--- bio-common/src/main/kotlin/bio/relatives/common/assembler/impl/GenomeAssemblerImpl.kt
+++ bio-common/src/main/kotlin/bio/relatives/common/assembler/impl/GenomeAssemblerImpl.kt
@@ -2,13 +2,32 @@ package bio.relatives.common.assembler.impl
 
 import bio.relatives.common.assembler.AssemblyCtx
 import bio.relatives.common.assembler.GenomeAssembler
+import bio.relatives.common.assembler.GenomeAssemblyResult
+import bio.relatives.common.assembler.GenomeAssemblyTask
+import bio.relatives.common.assembler.RegionAssembler
+import bio.relatives.common.model.Feature
 import bio.relatives.common.model.RegionBatch
-import bio.relatives.common.processor.AssemblyProcessor
-import kotlinx.coroutines.*
+import bio.relatives.common.model.RoleAware.Role
+import bio.relatives.common.parser.RegionParser
+import com.shvatov.processor.CoroutineScopeAware
+import com.shvatov.processor.config.TaskProcessorConfiguration
+import com.shvatov.processor.impl.TaskProcessorImpl
+import com.shvatov.processor.use
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.ObsoleteCoroutinesApi
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.launch
+import org.apache.commons.pool.BasePoolableObjectFactory
+import org.apache.commons.pool.impl.GenericObjectPool
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.util.EnumMap
 import java.util.concurrent.Executors
+import kotlin.coroutines.EmptyCoroutineContext
 
 /**
  * @author shvatov
@@ -20,14 +39,19 @@ class GenomeAssemblerImpl(
      * Assembling context, which contains singletons and some additional data,
      * required for the assembling. Will be used on lower levels of the assembling.
      */
-    private val assembleCtx: AssemblyCtx
+    private val assembleCtx: AssemblyCtx,
+
+    /**
+     * Scope of the parent coroutine this assembler is called from.
+     */
+    override val parentScope: CoroutineScope?
 ) : GenomeAssembler {
 
     /**
      * Parent scope for the execution of all the assembling coroutines.
      */
-    private val scope = CoroutineScope(
-        SupervisorJob() +
+    override val scope = CoroutineScope(
+        (parentScope?.coroutineContext ?: EmptyCoroutineContext) +
             CoroutineName("Assembler") +
             Executors.newFixedThreadPool(DEFAULT_ASSEMBLY_THREADS).asCoroutineDispatcher() +
             CoroutineExceptionHandler { ctx, exception ->
@@ -39,35 +63,100 @@ class GenomeAssemblerImpl(
     )
 
     /**
-     * Processor, which represents a separate coroutine, which will be used
-     * for the assembling of the genome.
+     * Processors, which will be used for the parallel execution
+     * of genome assembly tasks.
      */
-    private val processor = AssemblyProcessor(assembleCtx, scope)
+    private val processor = TaskProcessorImpl<Feature, RegionBatch>(
+        PROCESSOR_CONFIGURATION,
+        scope
+    )
 
-    override fun assemble(): ReceiveChannel<RegionBatch> =
+    /**
+     * Pool of [AssemblyTools], that are used for the genome assembly in processors.
+     */
+    private val assemblyToolsPool = GenericObjectPool(
+        AssemblyToolsFactory(), PROCESSOR_CONFIGURATION.subProcessorsNumber
+    )
+
+    override fun assemble(): ReceiveChannel<GenomeAssemblyResult> =
         processor.outputChannel.also {
             scope.launch {
-                val features = assembleCtx
-                    .featureParser
-                    .parseFeatures(assembleCtx.featureFilePath)
-
-                for (feature in features) {
-                    processor.inputChannel.send(feature)
+                processor.use {
+                    with(assembleCtx) {
+                        val features = featureParser.parseFeatures(assembleCtx.featureFilePath)
+                        for (feature in features) {
+                            submit(
+                                GenomeAssemblyTask(feature) {
+                                    val tools = assemblyToolsPool.borrowObject()
+                                    try {
+                                        assembleGenomes(it, tools)
+                                    } finally {
+                                        assemblyToolsPool.returnObject(tools)
+                                    }
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
 
     override fun close() {
-        // close the processor
-        processor.close()
+        assemblyToolsPool.close()
+    }
 
-        // cancel the scope
-        scope.cancel()
+    private fun assembleGenomes(feature: Feature, tools: AssemblyTools): RegionBatch {
+        val assemblyResults = assembleCtx.bamFilePaths.keys.map { role ->
+            val (parser, assembler) = tools.parserByRole.getValue(role) to tools.assembler
+            val records = parser.parseRegion(feature)
+            assembler.assemble(role, feature, records)
+        }.associateByTo(EnumMap(Role::class.java)) { it.role }
+
+        return RegionBatch(feature, assemblyResults)
+    }
+
+    /**
+     * Utils data class, which is used to store parser and assembler that
+     * are associated with one of the people we are working with.
+     */
+    private data class AssemblyTools(
+        val parserByRole: Map<Role, RegionParser>,
+        val assembler: RegionAssembler
+    )
+
+    /**
+     * Factory, which is used to add [AssemblyTools] to the pool.
+     */
+    private inner class AssemblyToolsFactory : BasePoolableObjectFactory<AssemblyTools>() {
+        override fun makeObject() = with(assembleCtx) {
+            AssemblyTools(
+                parserByRole = bamFilePaths.mapValues { regionParserFactory.create(it.value) },
+                assembler = regionAssemblerFactory.create()
+            )
+        }
+
+        override fun destroyObject(obj: AssemblyTools) {
+            obj.parserByRole.forEach {
+                it.value.close()
+            }
+            super.destroyObject(obj)
+        }
     }
 
     private companion object {
         val LOG: Logger = LoggerFactory.getLogger(GenomeAssemblerImpl::class.java)
 
+        /**
+         * Number of threads to be used by the assembler coroutine dispatcher.
+         */
         const val DEFAULT_ASSEMBLY_THREADS = 3
+
+        /**
+         * Default [TaskProcessorImpl] configuration.
+         * TODO: at some point make it editable by the user
+         */
+        val PROCESSOR_CONFIGURATION = TaskProcessorConfiguration(
+            exceptionHandler = CoroutineScopeAware.exceptionHandler(LOG),
+        )
     }
 }
